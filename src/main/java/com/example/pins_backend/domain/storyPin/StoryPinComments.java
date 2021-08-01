96fb71257d727506307b6495abf8684be2f41572
bangjinhyuk <jinhyuk0103@gmail.com>
2021-08-01 18:59:40 +0900

    [ADD] Directory & Entity classes

---
+++ src/main/java/com/example/pins_backend/domain/storyPin/StoryPinComments.java
@@ -0,0 +1,21 @@
+package com.example.pins_backend.domain.storyPin;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by bangjinhyuk on 2021/08/01.
+ */
+@Data
+@NoArgsConstructor
+@Entity
+public class StoryPinComments {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
---
+++ src/main/java/com/example/pins_backend/domain/user/User.java
@@ -0,0 +1,22 @@
+package com.example.pins_backend.domain.user;
+
+import com.sun.javafx.geom.transform.Identity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by bangjinhyuk on 2021/08/01.
+ */
+@Data
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
