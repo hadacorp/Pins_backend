555abcec511c78bbd4889d4041c8f215821e2d46
bangjinhyuk <jinhyuk0103@gmail.com>
2021-08-01 18:19:24 +0900

    [ADD] Create Spring Boot

---
+++ src/main/java/com/example/pins_backend/PinsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pins_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinsBackendApplication.class, args);
+    }
+
+}
