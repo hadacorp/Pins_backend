601b3db749e8176de4d866421d42b51c2761dee9
bangjinhyuk <jinhyuk0103@gmail.com>
2021-08-03 23:47:13 +0900

    [Add] Mysql structure

--- src/main/java/com/example/pins_backend/domain/user/User.java
+++ src/main/java/com/example/pins_backend/domain/user/User.java
@@ -1,10 +1,10 @@
 package com.example.pins_backend.domain.user;
 
-import com.sun.javafx.geom.transform.Identity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
