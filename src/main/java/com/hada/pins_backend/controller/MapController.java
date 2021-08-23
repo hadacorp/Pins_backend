7f0349015688aab4aca2fc0417389acd3948dd15
bangjinhyuk <jinhyuk0103@gmail.com>
2021-08-23 17:39:28 +0900

    test commit

--- src/main/java/com/hada/pins_backend/controller/MapController.java
+++ src/main/java/com/hada/pins_backend/controller/MapController.java
@@ -1,14 +1,7 @@
 package com.hada.pins_backend.controller;
 
-import com.hada.pins_backend.domain.meetingPin.MeetingPin;
-import com.hada.pins_backend.domain.user.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
