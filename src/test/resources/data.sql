2cff023278d52821a969863fd3cdf5594257919d
bangjinhyuk <jinhyuk0103@gmail.com>
2021-08-23 22:26:00 +0900

    Update mysql url

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,6 +1,6 @@
-insert into user (`id`, `name`, `nick_name`, `res_red_number`, `age`,`gender`,`image`) values (1, 'bang', 'bbangi', 9801031, 24,'Male','http://...');
-
-insert into user (`id`, `name`, `nick_name`, `res_red_number`, `age`,`gender`,`image`) values (2, 'park', 'ppark', 9302011, 28,'Male','http://...');
-
-insert into user (`id`, `name`, `nick_name`, `res_red_number`, `age`,`gender`,`image`) values (3, 'kim', 'kiki', 9202011, 29,'Female','http://...');
-
+-- insert into user (`id`, `name`, `nick_name`, `res_red_number`, `age`,`gender`,`image`) values (1, 'bang', 'bbangi', 9801031, 24,'Male','http://...');
+--
+-- insert into user (`id`, `name`, `nick_name`, `res_red_number`, `age`,`gender`,`image`) values (2, 'park', 'ppark', 9302011, 28,'Male','http://...');
+--
+-- insert into user (`id`, `name`, `nick_name`, `res_red_number`, `age`,`gender`,`image`) values (3, 'kim', 'kiki', 9202011, 29,'Female','http://...');
+--
