a74042f1fdf8b10f907da6c1a920bb444556e385
bangjinhyuk <jinhyuk0103@gmail.com>
2021-08-27 17:42:25 +0900

    feat:: Update application & Delete data.sql

--- build/resources/test/data.sql
+++
@@ -1,6 +0,0 @@
-insert into user (`id`, `name`, `nick_name`, `res_red_number`, `age`,`gender`,`image`) values (1, 'bang', 'bbangi', 9801031, 24,'Male','http://...');
-
-insert into user (`id`, `name`, `nick_name`, `res_red_number`, `age`,`gender`,`image`) values (2, 'park', 'ppark', 9302011, 28,'Male','http://...');
-
-insert into user (`id`, `name`, `nick_name`, `res_red_number`, `age`,`gender`,`image`) values (3, 'kim', 'kiki', 9202011, 29,'Female','http://...');
-
