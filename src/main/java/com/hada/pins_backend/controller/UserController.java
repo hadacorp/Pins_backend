b0dabd05c6f721f6ebd155b722106c05a804d671
bangjinhyuk <jinhyuk0103@gmail.com>
2021-08-28 18:46:10 +0900

    refactor:: Update UserController RequestMapping 어노테이션 추가

--- src/main/java/com/hada/pins_backend/controller/UserController.java
+++ src/main/java/com/hada/pins_backend/controller/UserController.java
@@ -1,31 +1,20 @@
 package com.hada.pins_backend.controller;
 
-import com.hada.pins_backend.config.JwtTokenProvider;
-import com.hada.pins_backend.domain.Gender;
-import com.hada.pins_backend.domain.meetingPin.MeetingPin;
-import com.hada.pins_backend.domain.meetingPin.MeetingPinRepository;
-import com.hada.pins_backend.domain.user.User;
-import com.hada.pins_backend.domain.user.UserRepository;
 import com.hada.pins_backend.dto.user.NicknameDto;
 import com.hada.pins_backend.dto.user.UserLoginForm;
 import com.hada.pins_backend.dto.user.request.JoinUserRequest;
 import com.hada.pins_backend.dto.user.response.JoinUserResponse;
 import com.hada.pins_backend.dto.user.response.LoginUserResponse;
-import com.hada.pins_backend.service.CustomUserDetailService;
 import com.hada.pins_backend.service.UserService;
-import com.hada.pins_backend.service.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.Collections;
-import java.util.Map;
+
 
 /**
  * Created by bangjinhyuk on 2021/08/12.
@@ -34,31 +23,32 @@ import java.util.Map;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/users")
 public class UserController {
 //    private final JwtTokenProvider jwtTokenProvider;
     private final UserService userService;
 
 
     // 회원가입
-    @PostMapping("/users/join")
+    @PostMapping("/join")
     public ResponseEntity<JoinUserResponse> join(@RequestBody @Valid JoinUserRequest userDto) {
         return userService.insertUser(userDto);
     }
 
     //가입 여부 확인
-    @PostMapping("/users/old-user")
+    @PostMapping("/old-user")
     public Boolean oldUser(@RequestBody @Valid UserLoginForm userLoginForm){
         return userService.checkOldUser(userLoginForm);
     }
 
     //닉네임 중복 확인
-    @PostMapping("/users/nickname")
+    @PostMapping("/nickname")
     public Boolean nickname(@RequestBody @Valid NicknameDto nickname){
         return userService.checkNickname(nickname.getNickname());
     }
 
     // 로그인
-    @PostMapping("/users/login")
+    @PostMapping("/login")
     public ResponseEntity<LoginUserResponse> login(@RequestBody @Valid UserLoginForm userLoginForm) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.login(userLoginForm));
     }
