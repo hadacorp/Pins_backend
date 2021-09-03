dddd0232f534e6b8e2ef7284df12ecd2b00795fa
bangjinhyuk <jinhyuk0103@gmail.com>
2021-09-03 18:29:50 +0900

    feat:: aws S3 환경설정 및 config 파일 생성

--- src/main/java/com/hada/pins_backend/PinsBackendApplication.java
+++ src/main/java/com/hada/pins_backend/PinsBackendApplication.java
@@ -2,12 +2,19 @@ package com.hada.pins_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class PinsBackendApplication {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "classpath:aws.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(PinsBackendApplication.class, args);
+        new SpringApplicationBuilder(PinsBackendApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 }
