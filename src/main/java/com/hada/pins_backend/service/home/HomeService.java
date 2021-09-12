71a454d7bfb42eb78526a35cb5e71b112a6d509e
bangjinhyuk <jinhyuk0103@gmail.com>
2021-09-13 00:15:20 +0900

    feat:: 핀, 카드뷰 분리 및 카드뷰 로딩, 정렬 기능 추가

--- src/main/java/com/hada/pins_backend/service/home/HomeService.java
+++ src/main/java/com/hada/pins_backend/service/home/HomeService.java
@@ -1,6 +1,7 @@
 package com.hada.pins_backend.service.home;
 
 import com.hada.pins_backend.domain.user.User;
+import com.hada.pins_backend.dto.home.response.HomeCardViewResponse;
 import com.hada.pins_backend.dto.home.response.HomeLocationResponse;
 import com.hada.pins_backend.dto.home.response.HomePinResponse;
 import org.springframework.http.ResponseEntity;
@@ -14,4 +15,6 @@ public interface HomeService {
     ResponseEntity<List<HomePinResponse>>loadPin(String phoneNum, double latitude, double logitude, String meetingPinCategory, String meetDate, String meetTime, String meetGender, String meetAge, String communityPinCategory, String storyPinCategory);
     ResponseEntity<List<HomePinResponse>> searchPin(String phoneNum, String keyword, double latitude, double longitude, String meetingPinCategory, String meetDate, String meetTime, String meetGender, String meetAge, String communityPinCategory, String storyPinCategory);
     ResponseEntity<List<HomeLocationResponse>> searchLocation(String keyword);
+    ResponseEntity<List<HomeCardViewResponse>> loadCardView(String phoneNum, String pinType, Long pinDBId, String meetingPinCategory, String meetDate, String meetTime, String meetGender, String meetAge, String communityPinCategory, String storyPinCategory);
+    ResponseEntity<List<HomeCardViewResponse>> searchCardView(String phoneNum, String pinType, Long pinDBId, String keyword, String meetingPinCategory, String meetDate, String meetTime, String meetGender, String meetAge, String communityPinCategory, String storyPinCategory);
 }
