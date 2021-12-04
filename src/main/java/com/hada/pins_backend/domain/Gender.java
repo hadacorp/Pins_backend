21d0330a81bf1331789a5f0351b74795d1aded29
bangjinhyuk <jinhyuk0103@gmail.com>
2021-12-04 19:57:39 +0900

    feat:: gender to String

--- src/main/java/com/hada/pins_backend/domain/Gender.java
+++ src/main/java/com/hada/pins_backend/domain/Gender.java
@@ -4,8 +4,11 @@ package com.hada.pins_backend.domain;
  * Created by bangjinhyuk on 2021/08/04.
  */
 public enum Gender {
+    Male, Female, Both;
 
-    Male,
-    Female,
-    Both
+    public static String convert2Korean(Gender gender) {
+        if(gender == Gender.Male) return "남자";
+        else if(gender == Gender.Female) return "여자";
+        else return "성별 무관";
+    }
 }
