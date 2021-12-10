4b1f051e0f67fbe93537b6fe4c4c7bde795bcd22
bangjinhyuk <jinhyuk0103@gmail.com>
2021-12-10 23:13:36 +0900

    feat:: StompWebSocketConfig 추가

---
+++ src/main/java/com/hada/pins_backend/config/StompWebSocketConfig.java
@@ -0,0 +1,28 @@
+package com.hada.pins_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * Created by bangjinhyuk on 2021/12/10.
+ */
+@EnableWebSocketMessageBroker
+@Configuration
+public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/stomp")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/pub");
+        registry.enableSimpleBroker("/sub");
+    }
+}
